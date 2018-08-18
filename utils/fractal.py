222d7d8da2b1225d6aa644e0e21ae433b523acc1
Tristan de Cacqueray <tristanC@wombatt.eu>
2018-08-18 03:32:21 +0000

    Add orbit kernel

--- utils/fractal.py
+++ utils/fractal.py
@@ -36,6 +36,145 @@ z = cdouble_log(z);
 
 
 DEFAULT_KERNELS = {
+    "orbit-rgb": """
+__constant uint gradient[] = {{{gradient_values}}};
+#define PYOPENCL_DEFINE_CDOUBLE 1
+#include <pyopencl-complex.h>
+#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+__kernel void compute(
+    __global double2 *plane,
+    __global uint *pixels,
+    char const julia,
+    uint const max_iter,
+    double const gradient_frequency,
+    double const c_real,
+    double const c_imag
+    {kernel_params}
+) {{
+    int gid = get_global_id(0);
+    cdouble_t z;
+    cdouble_t z2;
+    cdouble_t c;
+
+    if (julia) {{
+        z = cdouble_new({pos_x}, {pos_y});
+        c = cdouble_new(c_real, c_imag);
+    }} else {{
+        z = cdouble_new(0, 0);
+        c = cdouble_new({pos_x}, {pos_y});
+    }}
+    {kernel_variables}
+    double escape = {escape_distance};
+    double modulus = 0.0f;
+    cdouble_t orbit;
+    double orbit_modulus = 0.0f;
+    orbit = cdouble_new(mod, mod2);
+
+    double mean = 0.0f;
+    double rdistance = escape;
+    double gdistance = escape;
+    int iter;
+    pixels[gid] = 0x00000000;
+    for (iter = 0; iter < max_iter; iter++) {{
+        {formula}
+
+        orbit_modulus = fabs(z.imag - orbit.imag);
+
+        if (orbit_modulus < trap) {{
+            pixels[gid] = gradient[(int)(
+                ((orbit_modulus / trap) * {gradient_length}/10)) + 20];
+            break ;
+        }}
+        orbit_modulus = fabs(z.real - orbit.real);
+
+        if (orbit_modulus < trap) {{
+            pixels[gid] = gradient[(int)(
+                ((orbit_modulus / trap) * {gradient_length} / 10))];
+            // pixels[gid] = 0xff000000 | ((int)(0xff * orbit_modulus / trap) << 8);
+            break ;
+        }}
+
+        modulus = cdouble_abs(z);
+        if (modulus > escape) {{
+            modulus = iter - log(log(modulus)) / log(2.0f) +
+                             log(log(escape)) / log(2.0f);
+            modulus = modulus / (double)max_iter;
+            pixels[gid] = gradient[(int)(
+                (modulus * {gradient_length} * gradient_frequency)) %
+                {gradient_length}];
+            break;
+        }}
+    }}
+}}
+""",
+
+    "orbit-gradient": """
+__constant uint gradient[] = {{{gradient_values}}};
+#define PYOPENCL_DEFINE_CDOUBLE 1
+#include <pyopencl-complex.h>
+#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+__kernel void compute(
+    __global double2 *plane,
+    __global uint *pixels,
+    char const julia,
+    uint const max_iter,
+    double const gradient_frequency,
+    double const c_real,
+    double const c_imag
+    {kernel_params}
+) {{
+    int gid = get_global_id(0);
+    cdouble_t z;
+    cdouble_t z2;
+    cdouble_t c;
+
+    if (julia) {{
+        z = cdouble_new({pos_x}, {pos_y});
+        c = cdouble_new(c_real, c_imag);
+    }} else {{
+        z = cdouble_new(0, 0);
+        c = cdouble_new({pos_x}, {pos_y});
+    }}
+    {kernel_variables}
+    double escape = {escape_distance};
+    double modulus = 0.0f;
+    cdouble_t orbit;
+    double orbit_modulus = 0.0f;
+    orbit = cdouble_new(mod, mod2);
+
+    double mean = 0.0f;
+    double distance = escape;
+    int iter;
+    for (iter = 0; iter < max_iter; iter++) {{
+        {formula}
+
+        orbit_modulus = fabs(z.imag - orbit.imag);
+        //mean += orbit_modulus;
+
+        if (orbit_modulus < distance) {{
+            distance = orbit_modulus;
+        }}
+        orbit_modulus = fabs(z.real - orbit.real);
+
+        if (orbit_modulus < distance) {{
+            distance = orbit_modulus;
+        }}
+
+        modulus = cdouble_abs(z);
+        if (modulus > escape) {{
+            break;
+        }}
+    }}
+    distance = sqrt(distance);
+    pixels[gid] = gradient[(int)(
+          (distance * {gradient_length} * gradient_frequency)) %
+          {gradient_length}]
+        ;
+
+}}
+""",
     "escape-time-gradient": """
 __constant uint gradient[] = {{{gradient_values}}};
 #define PYOPENCL_DEFINE_CDOUBLE 1
